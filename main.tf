e2ad0efc77b040f7fe54b79ea5f18e1861a4b8ad
Hwlann <55738252+Hwlann@users.noreply.github.com>
2021-01-25 14:36:25 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "security_group_instaces_loic" {
   count         = var.create_instance ? var.instance_number : 0
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
@@ -31,4 +31,28 @@ resource "aws_instance" "web" {
   tags = {
     Name = var.instance_name
   }
+}
+resource "aws_security_group" "security_group_instaces_loic" {
+    name = "security_group_instaces_loic"
+
+ ingress {
+     from_port   = 22
+     to_port     = 22
+     protocol    = "tcp"
+     cidr_blocks     = ["0.0.0.0/0"]
+   }
+
+ ingress {
+     from_port   = 80
+     to_port     = 80
+     protocol    = "tcp"
+     cidr_blocks     = ["0.0.0.0/0"]
+   }
+
+ egress {
+     from_port       = 0
+     to_port         = 0
+     protocol        = "-1"
+     cidr_blocks     = ["0.0.0.0/0"]
+   }
 }
